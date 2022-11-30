88d3bfa3c971110eb983b469cfb3e6999a06445f
MyDuyen2 <117999338+MyDuyen2@users.noreply.github.com>
2022-11-30 19:23:37 +0700

    Initial commit

---
+++ app/src/main/java/com/lethimyduyen/myapplication/MainActivity.java
@@ -0,0 +1,54 @@
+package com.lethimyduyen.myapplication;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
+    HomeFragment homeFragment = new HomeFragment();
+    YethichFragment yethichFragment = new YethichFragment();
+    taikhoanFragment taikhoanFragment = new taikhoanFragment();
+    KhamPhaFragment khamPhaFragment = new KhamPhaFragment();
+    giohangFragment giohangFragment = new giohangFragment();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_nav);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.home:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,homeFragment).commit();
+                        return true;
+                    case R.id.find:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,khamPhaFragment).commit();
+                        return true;
+                    case R.id.shopping:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,giohangFragment).commit();
+                        return true;
+                    case R.id.heart:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,yethichFragment).commit();
+                        return true;
+                    case R.id.setting:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,taikhoanFragment).commit();
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+
+
+}
